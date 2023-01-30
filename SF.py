5e5830ba24a105dd314ae899421954674301360b
meshkova-if <121893822+meshkova-if@users.noreply.github.com>
2023-01-30 14:34:12 +0500

    up

--- SF.py
+++ SF.py
@@ -1,5 +1,9 @@
-from collections import defaultdict
-groups = defaultdict(list)
-students = [('Ivanov',1),('Smirnov',4),('Petrov',3),('Kuznetsova',1),
-            ('Nikitina',2),('Markov',3),('Pavlov',2)]
-for student 
+import numpy as np
+#b = np.int16(16)
+#print(np.iinfo(b))#Какие минимальное и максимальное числа можно сохранить в 16 бит?
+#c = np.int64()
+#print(np.iinfo(c))
+#n = np.uint64(16)
+#print(np.iinfo(n))
+z, step = np.linspace(-6,20,60, endpoint=True, retstep=True)
+print(step.round(2))
