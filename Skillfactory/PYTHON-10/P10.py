94bf1631113265edd4c8f5e391379ab479fbced4
meshkova-if <121893822+meshkova-if@users.noreply.github.com>
2023-02-06 11:36:00 +0500

    up

--- Skillfactory/PYTHON-10/P10.py
+++ Skillfactory/PYTHON-10/P10.py
@@ -1,4 +1,4 @@
-import pandas as pd
+#import pandas as pd
 #def create_medications(names, counts):
     #medications = pd.Series(index=names, data=counts)
     #return medications
@@ -7,25 +7,25 @@ import pandas as pd
 #names=['chlorhexidine', 'cyntomycin', 'afobazol']
 #counts=[15, 18, 7]
 #print(create_medications(names = ['chlorhexidine', 'cyntomycin', 'afobazol'], counts = [26, 18, 7]))
-def create_companyDF(income, expenses, years):
-    df = pd.DataFrame({
-        'Income': income,
-        'Expenses': expenses
-        },
-        index = years
-    )
-    return df
-incomes = [478, 512, 196]
-expenses = [156, 130, 270]
-years = [2018, 2019, 2020]
+#def create_companyDF(income, expenses, years):
+    #df = pd.DataFrame({
+        #'Income': income,
+        #'Expenses': expenses
+        #},
+        #index = years
+    #)
+    #return df
+#incomes = [478, 512, 196]
+#expenses = [156, 130, 270]
+#years = [2018, 2019, 2020]
 
-def get_profit(df, year):
-    if year in df.index:
-        profit = df.loc[year, 'incomes']-df.loc[year,'expenses']
-    else:
-        profit=None
-        return profit
+#def get_profit(df, year):
+    #if year in df.index:
+        #profit = df.loc[year, 'incomes']-df.loc[year,'expenses']
+    #else:
+        #profit=None
+        #return profit
 
 
-print(get_profit(df=incomes,expenses,years))
+#print(get_profit(df=incomes,expenses,years))
 
