32aaaae23939f925f476a2dd8135e25de44d20f7
meshkova-if <121893822+meshkova-if@users.noreply.github.com>
2023-02-06 10:05:27 +0500

    up

--- SF.py
+++ SF.py
@@ -1,14 +1,14 @@
-import numpy as np
-np.random.seed(2021)
-simple = np.random.rand()
-randoms = np.random.uniform(-150,2021,size=120)
-table = np.random.randint(1,101, size=(3,2))
-even = np.arange(2,17,2)
-mix = even
-np.random.shuffle(mix)
-select = np.random.choice(even, size=3, replace=False)
-triplet = np.random.permutation(select)
-print(triplet)
-simplelist = [19, 242, 14, 152, 142, 1000]
-n = sum(simplelist)/len(simplelist)
-print(n)
+#import numpy as np
+#np.random.seed(2021)
+#simple = np.random.rand()
+#randoms = np.random.uniform(-150,2021,size=120)
+#table = np.random.randint(1,101, size=(3,2))
+#even = np.arange(2,17,2)
+#mix = even
+#np.random.shuffle(mix)
+#select = np.random.choice(even, size=3, replace=False)
+#triplet = np.random.permutation(select)
+#print(triplet)
+#simplelist = [19, 242, 14, 152, 142, 1000]
+#n = sum(simplelist)/len(simplelist)
+#print(n)
--- Skillfactory/PYTHON-10/P10.py
+++ Skillfactory/PYTHON-10/P10.py
@@ -1,11 +1,12 @@
 import pandas as pd
-def create_medications(names, counts):
-    medications = pd.Series(index=names, data=counts)
-    return medications
-def get_percent(medications, name):
-    return(medications.loc[name]/sum(medications) * 100)
-
-
-names=['chlorhexidine', 'cyntomycin', 'afobazol']
-counts=[15, 18, 7]
-print(create_medications(names = ['chlorhexidine', 'cyntomycin', 'afobazol'], counts = [26, 18, 7]))
+#def create_medications(names, counts):
+    #medications = pd.Series(index=names, data=counts)
+    #return medications
+#def get_percent(medications, name):
+    #return(medications.loc[name]/sum(medications) * 100)
+#names=['chlorhexidine', 'cyntomycin', 'afobazol']
+#counts=[15, 18, 7]
+#print(create_medications(names = ['chlorhexidine', 'cyntomycin', 'afobazol'], counts = [26, 18, 7]))
+#def create_companyDF(incomes, expenses, years):
+company_DF = pd.DataFrame([478, 512, 196], [156, 130, 270], columns=[2018, 2019, 2020])
+print(company_DF)
