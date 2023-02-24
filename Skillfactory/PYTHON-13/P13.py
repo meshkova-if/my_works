688ad59580d1bc4b9fbe3d8932aa96d9e15c945e
meshkova-if <121893822+meshkova-if@users.noreply.github.com>
2023-02-25 00:19:57 +0500

    up

--- Skillfactory/PYTHON-13/P13.py
+++ Skillfactory/PYTHON-13/P13.py
@@ -19,4 +19,25 @@ orders_products['col_pr'] = (orders_products['Количество']*orders_prod
 #print(orders_products[orders_products['Оплачен'] == 'Да'].groupby('ID Покупателя')['col_pr'].sum().sort_values(ascending=False))
 covid_data = pd.read_csv('data/covid_data.csv')
 covid_data.head()
-print(covid_data)
+#print(covid_data.columns)
+vaccinations_data = pd.read_csv('data/country_vaccinations.csv')
+vaccinations_data = vaccinations_data[
+    ['country', 'date', 'total_vaccinations', 
+     'people_vaccinated', 'people_vaccinated_per_hundred',
+     'people_fully_vaccinated', 'people_fully_vaccinated_per_hundred',
+     'daily_vaccinations', 'vaccines']
+]
+covid_data = covid_data.groupby(['date','country'], as_index=False)[['confirmed', 'deaths', 'recovered']].sum()
+covid_data['date']= pd.to_datetime(covid_data['date'])
+covid_data['active']= covid_data['confirmed'] - covid_data['deaths'] - covid_data['recovered']#еще болеют
+covid_data = covid_data.sort_values(by= ['country', 'date'])
+covid_data['daily_confirmed'] = covid_data.groupby('country')['confirmed'].diff()#прирост заболеваемости
+covid_data['daily_deaths'] = covid_data.groupby('country')['deaths'].diff()#прирост смертности
+covid_data['daily_recovered'] = covid_data.groupby('country')['recovered'].diff()#прирост выздоровевших
+vaccinations_data['date'] = pd.to_datetime(vaccinations_data['date'])
+#print(f"Данные о заболеваемости предоставлены от {vaccinations_data['date'].min()} до  {vaccinations_data['date'].max()}")
+covid_df = covid_data.merge(vaccinations_data, how='left', on=['date', 'country'])
+covid_df['death_rate'] = covid_df['deaths'] /covid_df['confirmed']*100
+covid_df['recover_rate'] = covid_df['recovered']/covid_df['confirmed']*100
+#print(round(covid_df[covid_df['country'] == 'United States']['death_rate'].max(), 2))
+print(round(covid_df[covid_df['country']=='Russia']['recover_rate'].mean(), 2))
